46ef16522270cb47a121e75067e5eb0a240e9087
Lucky Kumar <luckykumar72600@gmail.com>
2026-09-05 06:28:12 -0700

    677A

---
+++ 800/677A.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,h;
+    cin>>n>>h;
+
+    int a[n];
+    
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+
+    int count=0;
+    for(int i=0; i<n; i++){
+        if(a[i]>h){
+            count+=2;
+        }
+        else{
+            count++;
+        }
+    }
+
+    cout<<count;
+    return 0;
+}
